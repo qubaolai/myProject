1a28a09bb18f09c18f593a7f6248e4265dd6afea
qubaolai <2424419428@qq.com>
2019-11-11 07:18:02 +0800

    提交sql

--- 02设计相关/数据库设计/表设计/SQL/hr.sql
+++ 02设计相关/数据库设计/表设计/SQL/hr.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50619
  File Encoding         : 65001
 
- Date: 06/10/2019 09:32:59
+ Date: 11/11/2019 07:16:51
 */
 
 SET NAMES utf8mb4;
@@ -22,8 +22,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `attendance`;
 CREATE TABLE `attendance`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `employee_number` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `employee_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `day` date NULL DEFAULT NULL,
   `time_type` enum('上午','下午','加班') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `start_time` time(0) NULL DEFAULT NULL,
@@ -33,8 +33,7 @@ CREATE TABLE `attendance`  (
   `work_type` enum('上班','请假') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `att_emp_fk`(`employee_number`) USING BTREE,
-  CONSTRAINT `att_emp_fk` FOREIGN KEY (`employee_number`) REFERENCES `employee` (`employee_number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `att_emp_fk`(`employee_number`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '签到记录' ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -42,16 +41,20 @@ CREATE TABLE `attendance`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `department`;
 CREATE TABLE `department`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `department_number` int(10) NULL DEFAULT NULL,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `manager` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `manager` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `telephone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `department_number`(`department_number`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+INSERT INTO `department` VALUES ('0a0d0aa5acfd49369e05e945310a0d23', 1, '骨伤外科', '9004f903dd8541e685fb2328fc1a872b', '52373', '骨伤外科部，经理：asd，电话：52373');
 
 -- ----------------------------
 -- Table structure for employee
@@ -59,38 +62,39 @@ CREATE TABLE `department`  (
 DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee`  (
   `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `employee_number` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `employee_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `gender` enum('男','女') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `gender` enum('男','女') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
   `birthday` date NULL DEFAULT NULL,
   `telephone` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   `address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `education` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `education` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '学历',
   `department_number` int(10) NULL DEFAULT NULL,
-  `position_number` int(10) NULL DEFAULT NULL,
-  `in_time` date NULL DEFAULT NULL,
+  `position_number` int(10) NULL DEFAULT NULL COMMENT '职位编号',
+  `in_time` date NULL DEFAULT NULL COMMENT '入职',
   `password` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '备注',
   `deviceId` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '设备号用于验证异地登陆',
+  `role` int(10) NULL DEFAULT 1 COMMENT '身份',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `employee_number`(`employee_number`) USING BTREE,
-  INDEX `emp_dep_fk`(`department_number`) USING BTREE,
-  CONSTRAINT `emp_dep_fk` FOREIGN KEY (`department_number`) REFERENCES `department` (`department_number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `emp_dep_fk`(`department_number`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of employee
 -- ----------------------------
-INSERT INTO `employee` VALUES ('27afe6d9dd9047229fdfc968e549ab05', 'asd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'asd', NULL, NULL);
+INSERT INTO `employee` VALUES ('', '', NULL, NULL, NULL, '', '', NULL, '', NULL, NULL, NULL, NULL, '', NULL, 1);
+INSERT INTO `employee` VALUES ('9004f903dd8541e685fb2328fc1a872b', '7815696ecbf1c96e6894b779456d330e', '曲宝来', '男', '2019-10-31', '13894658959', 'qblsiwen@163.com', '吉林省大安市', '本科', 1, 1, '2019-10-31', '1bd5d61d89d2115c2ddc88fc5ebcc97f', NULL, '11', 0);
 
 -- ----------------------------
 -- Table structure for history
 -- ----------------------------
 DROP TABLE IF EXISTS `history`;
 CREATE TABLE `history`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `employee_number` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `employee_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `gender` enum('男','女') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `birthday` date NULL DEFAULT NULL,
@@ -103,15 +107,11 @@ CREATE TABLE `history`  (
   `department_number` int(10) NULL DEFAULT NULL,
   `position_number` int(10) NULL DEFAULT NULL COMMENT '职位',
   `status` enum('离职','在职','退休') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `home` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `his_emp_fk`(`employee_number`) USING BTREE,
   INDEX `his_dep_fk`(`department_number`) USING BTREE,
-  INDEX `his_pos_fk`(`position_number`) USING BTREE,
-  CONSTRAINT `his_emp_fk` FOREIGN KEY (`employee_number`) REFERENCES `employee` (`employee_number`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `his_dep_fk` FOREIGN KEY (`department_number`) REFERENCES `department` (`department_number`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `his_pos_fk` FOREIGN KEY (`position_number`) REFERENCES `position` (`position_number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `his_pos_fk`(`position_number`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '离休员工表' ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -119,8 +119,8 @@ CREATE TABLE `history`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `lea`;
 CREATE TABLE `lea`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `employee_number` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `employee_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `department_number` int(10) NULL DEFAULT NULL,
   `start_time` date NULL DEFAULT NULL,
   `end_time` date NULL DEFAULT NULL,
@@ -131,30 +131,44 @@ CREATE TABLE `lea`  (
   `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `lea_emp_fk`(`employee_number`) USING BTREE,
-  INDEX `lea_dep_fk`(`department_number`) USING BTREE,
-  CONSTRAINT `lea_emp_fk` FOREIGN KEY (`employee_number`) REFERENCES `employee` (`employee_number`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `lea_dep_fk` FOREIGN KEY (`department_number`) REFERENCES `department` (`department_number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `lea_dep_fk`(`department_number`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '请假表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for logs
+-- ----------------------------
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE `logs`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `employee_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `log` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `updateTime` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '密码修改日志' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of logs
+-- ----------------------------
+INSERT INTO `logs` VALUES ('7e77e0ed98bf457fa135d4149141272a', '9004f903dd8541e685fb2328fc1a872b', '密码错误', '2019-11-10 14:34:16', '2019-11-10 14:34:16');
+INSERT INTO `logs` VALUES ('84dfc1500ea54778a90fb4a51d328143', '9004f903dd8541e685fb2328fc1a872b', '密码错误', '2019-11-10 14:57:59', '2019-11-10 14:57:59');
 
 -- ----------------------------
 -- Table structure for move
 -- ----------------------------
 DROP TABLE IF EXISTS `move`;
 CREATE TABLE `move`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `employee_number` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `employee_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `before` int(10) NULL DEFAULT NULL COMMENT '调动前 对应职位表职务编号',
   `after` int(10) NULL DEFAULT NULL COMMENT '调动后 对应职位表职务编号',
   `time` datetime(0) NULL DEFAULT NULL,
-  `manager` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `manager` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `mov_emp_fk`(`employee_number`) USING BTREE,
   INDEX `mov_pos_fk_after`(`after`) USING BTREE,
-  INDEX `mov_pos_fk_before`(`before`) USING BTREE,
-  CONSTRAINT `mov_pos_fk_after` FOREIGN KEY (`after`) REFERENCES `position` (`position_number`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `mov_pos_fk_before` FOREIGN KEY (`before`) REFERENCES `position` (`position_number`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `mov_emp_fk` FOREIGN KEY (`employee_number`) REFERENCES `employee` (`employee_number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `mov_pos_fk_before`(`before`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '员工调动记录' ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -162,18 +176,16 @@ CREATE TABLE `move`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `overtime`;
 CREATE TABLE `overtime`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `department_number` int(10) NULL DEFAULT NULL,
-  `employee_number` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `employee_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `day` date NULL DEFAULT NULL,
   `start_time` time(0) NULL DEFAULT NULL,
   `end_time` time(0) NULL DEFAULT NULL,
   `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `ove_dep_fk`(`department_number`) USING BTREE,
-  INDEX `ove_emp_fk`(`employee_number`) USING BTREE,
-  CONSTRAINT `ove_dep_fk` FOREIGN KEY (`department_number`) REFERENCES `department` (`department_number`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `ove_emp_fk` FOREIGN KEY (`employee_number`) REFERENCES `employee` (`employee_number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `ove_emp_fk`(`employee_number`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '加班' ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -181,7 +193,8 @@ CREATE TABLE `overtime`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `position`;
 CREATE TABLE `position`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `department_number` int(10) NULL DEFAULT NULL,
   `position_number` int(10) NULL DEFAULT NULL,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `notes` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
@@ -190,21 +203,25 @@ CREATE TABLE `position`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '职位表' ROW_FORMAT = Compact;
 
 -- ----------------------------
+-- Records of position
+-- ----------------------------
+INSERT INTO `position` VALUES ('ee250b65481a4dd6a0055a080af59da4', 1, 1, '老大', '骨伤外科部职位');
+
+-- ----------------------------
 -- Table structure for rewards_punishment
 -- ----------------------------
 DROP TABLE IF EXISTS `rewards_punishment`;
 CREATE TABLE `rewards_punishment`  (
-  `id` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `employee_number` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `employee_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `type` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `reason` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '原因',
   `money` float(8, 0) NULL DEFAULT NULL,
   `time` datetime(6) NULL DEFAULT NULL,
-  `manager` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `manager` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `notes` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `employee_number`(`employee_number`) USING BTREE,
-  CONSTRAINT `rew_emp_fk` FOREIGN KEY (`employee_number`) REFERENCES `employee` (`employee_number`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `employee_number`(`employee_number`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '奖罚表' ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
