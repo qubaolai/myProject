71d86cca55d47ca3e01d0ffb373fc96e915fc5e2
qubaolai <2424419428@qq.com>
2019-12-14 16:16:37 +0800

    提交vue引用

--- 03源码/hr-client/src/router/index.js
+++ 03源码/hr-client/src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from '@/login'
-
+import login from '@/components/login'
+import user from '@/components/user'
+import hello from '@/components/HelloWorld'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Router)
+Vue.use(ElementUI)
 
 export default new Router({
   routes: [
+    
     {
       path: '/',
       name: 'login',
       component: login
-    }
+    },
+    {
+      path: '/',
+      name: 'user',
+      component: user
+    },
+    {
+      path: '/',
+      name: 'hello',
+      component: hello
+    },
   ]
 })
